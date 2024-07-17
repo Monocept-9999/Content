e200dda4300048a2568f9347a6e46aff3ebc2006
Sonu Kumar <sonu.kumar@maxlifeinsurance.com>
2024-07-17 17:44:02 +0530

    policy status task

---
+++ ContentMS/src/main/java/com/example/content/Entities/PolicyStatus.java
@@ -0,0 +1,7 @@
+package com.example.content.Entities;
+
+public enum PolicyStatus {
+    ACTIVE,
+    EXPIRED,
+    EXPIRING_SOON
+}
---
+++ ContentMS/src/main/java/com/example/content/Service/PolicyServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.content.Service;
+
+import com.example.content.Entities.Policy;
+import com.example.content.Entities.PolicyStatus;
+import com.example.content.Repository.PolicyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class PolicyServiceImpl implements PolicyService {
+
+    @Autowired
+    private PolicyRepository policyRepository;
+
+
+    @Override
+    public List<Policy> getExpiringSoonPolicies() {
+        return policyRepository.findAllByStatus(PolicyStatus.EXPIRING_SOON);
+    }
+
+    @Override
+    public List<Policy> getExpiredPolicies() {
+        return policyRepository.findAllByStatus(PolicyStatus.EXPIRED);
+    }
+
+    @Override
+    public List<Policy> getActivePolicies() {
+        return policyRepository.findAllByStatus(PolicyStatus.ACTIVE);
+    }
+
+    @Override
+    public Policy createPolicy(Policy policy) {
+        return policyRepository.save(policy);
+    }
+
+    @Override
+    public String deletePolicy(Long policyId) {
+        policyRepository.deleteById(policyId);
+        return "Policy Deleted Successfully";
+    }
+
+    /// this method will update policy status every day at midnight
+    @Scheduled(cron = "0 0 0 * * ?") // Runs every day at midnight
+    public void updatePolicyStatuses() {
+        List<Policy> policies = policyRepository.findAll();
+        LocalDate today = LocalDate.now();
+
+        for (Policy policy : policies) {
+            if (policy.getEndDate().isBefore(today)) {
+                policy.setStatus(PolicyStatus.EXPIRED);
+            } else if (policy.getEndDate().isEqual(today.plusDays(30))) {
+                policy.setStatus(PolicyStatus.EXPIRING_SOON);
+            } else {
+                policy.setStatus(PolicyStatus.ACTIVE);
+            }
+        }
+
+        policyRepository.saveAll(policies);
+    }
+}
